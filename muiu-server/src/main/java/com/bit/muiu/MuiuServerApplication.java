09db355da5a276b803d47c9084177b821e3309ba
ChungDaeun <daeunchungb2m@gmail.com>
2024-09-11 20:21:31 +0900

    240911 정다은 boot틀 만드는 중

---
+++ muiu-server/src/main/java/com/bit/muiu/MuiuServerApplication.java
@@ -0,0 +1,13 @@
+package com.bit.muiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuiuServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuiuServerApplication.class, args);
+    }
+
+}
