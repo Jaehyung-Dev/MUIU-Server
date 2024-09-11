825d4ed41d1e1de0c1460512ee7d19b4ee371c3f
ChungDaeun <daeunchungb2m@gmail.com>
2024-09-11 20:21:31 +0900

    240911 정다은 boot틀 만드는 중

---
+++ muiu-server/src/test/java/com/bit/muiu/MuiuServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bit.muiu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuiuServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
