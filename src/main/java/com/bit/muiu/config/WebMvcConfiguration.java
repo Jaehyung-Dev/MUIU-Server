f607d765f76f41f54f84f71eb3061a39df950adb
ChungDaeun <daeunchungb2m@gmail.com>
2024-10-01 12:03:20 +0900

    Modify WebMvcConfiguration.java

--- src/main/java/com/bit/muiu/config/WebMvcConfiguration.java
+++ src/main/java/com/bit/muiu/config/WebMvcConfiguration.java
@@ -1,4 +1,18 @@
 package com.bit.muiu.config;
 
-public class WebMvcConfiguration {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
 }
