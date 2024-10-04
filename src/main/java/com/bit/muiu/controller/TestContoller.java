90b8679c8e0f7a654eeea9bd5154d5eac536c4c0
daehwi <kimdaehwi990731@gmail.com>
2024-10-04 17:20:37 +0900

    https test

---
+++ src/main/java/com/bit/muiu/controller/TestContoller.java
@@ -0,0 +1,14 @@
+package com.bit.muiu.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestContoller {
+    @GetMapping("/test")
+    public String test() {
+        return "aaa";
+    }
+}
--- src/test/java/com/bit/muiu/MuiuServerApplicationTests.java
+++ src/test/java/com/bit/muiu/MuiuServerApplicationTests.java
@@ -3,7 +3,7 @@ package com.bit.muiu;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class MuiuServerApplicationTests {
 
     @Test
