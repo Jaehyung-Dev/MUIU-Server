c92eddadf530d3f2a92ea92388411104e8b21c25
ChungDaeun <daeunchungb2m@gmail.com>
2024-10-22 13:43:59 +0900

    Add MindColumnRepository.java, MindColumnFileRepository.java

---
+++ src/main/java/com/bit/muiu/repository/MindColumnFileRepository.java
@@ -0,0 +1,8 @@
+package com.bit.muiu.repository;
+
+import com.bit.muiu.entity.MindColumn;
+import com.bit.muiu.entity.MindColumnFile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MindColumnFileRepository extends JpaRepository<MindColumnFile, Long> {
+}
---
+++ src/main/java/com/bit/muiu/repository/MindColumnRepository.java
@@ -0,0 +1,7 @@
+package com.bit.muiu.repository;
+
+import com.bit.muiu.entity.MindColumn;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MindColumnRepository extends JpaRepository<MindColumn, Long> {
+}
